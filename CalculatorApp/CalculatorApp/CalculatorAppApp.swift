3d98a5790842d14500e443569ec21102f9098277
Brendan Losinski <88355167+blosinski@users.noreply.github.com>
2022-07-29 08:06:39 -0500

    Add files via upload
    Built a full functioning Calculator App.

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Brendan Losinski on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
